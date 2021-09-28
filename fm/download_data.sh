f702407eb2e6f34aabec49cb000d72a40db47d6f
Wirid-Lab Universidad Militar Nueva Granada <wirid-lab@unimilitar.edu.co>
2021-09-28 00:22:10 -0400

    Update download_data.sh

--- fm/download_data.sh
+++ fm/download_data.sh
@@ -1,1 +1,1 @@
-wget https://drive.google.com/uc?id=1QcVRK66GjHs89IJid3o8ZljwJpneQJ26&export=download
+wget -O espectro_94_9 https://drive.google.com/uc?id=1QcVRK66GjHs89IJid3o8ZljwJpneQJ26&export=download
